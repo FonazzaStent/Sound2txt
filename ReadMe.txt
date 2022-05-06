56d010e2acb9f19f8da76c303ad5abff60b0c3cf
Maurizio Crescenzo <92685677+GummerStreet@users.noreply.github.com>
2022-05-06 13:27:00 +0200

    Add files via upload

---
+++ ReadMe.txt
@@ -0,0 +1,23 @@
+Sound to Text 1.0.0
+
+The program will convert a soundfile to text, with the purpose of analyzing it to spot intelligible words. Use the program with songs, movie or TV dialogue, private recordings, to reveal the hidden text messages of the sound. The resulting text will be a long string of mostly repeating characters but, every so often, you'll notice an intelligible word, either spelled normally or with repeated letters, missing letters or anagramed letters. It takes a little allegoric-freudian psychological exegetic ability to elaborate the right chain of mind associations to infere a convincing interpretation of the resulting text. You may want to share the text with a psychologist to get a professional opinion on your interpretation, but sometimes the meaning will be surprisingly obvious.
+Because of my coding skills limitations, the program requires a little collaboration on the user side, as the sound file must be mandatorily in a certain format, obtainable by converting it through a sound editing program such as free and open source Audacity.
+
+Instructions:
+
+- open Audacity
+- open the sound file
+- Select - All
+- Normalize  - Set peak amplitude to 0.0 - OK
+- Set Project Rate (Hz) to 8000 Hz
+- Export - Export as WAV
+- Save as type - Other uncompressed files
+- Header: WAV (Microsoft)
+- Encoding: Unsigned 8-bit PCM
+- Save
+
+Now load the sound file into the program:
+
+- Open the program, click on Edit - Open or CTRL+O.
+
+The resulting text will appear in the text window. The window will display only the first 20.000 characters, which you can copy through File-Copy or by rigth clicking on it or CTRL-C. If you want to read the whole text you must save it, through the Save command in the File menu or CTRL-S.
---
+++ Sound2txt_1.0.0.py
@@ -0,0 +1,135 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import asksaveasfilename
+from tkinter import messagebox
+import base64
+import os
+import shutil
+
+#Create app window
+def create_app_window():
+        global top
+        global root
+        img=b'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABmJLR0QAGAAUAAy7SiC7AAAACXBIWXMAAC4jAAAuIwF4pT92AAAAB3RJTUUH5gUGCAofmLY2VAAAABl0RVh0Q29tbWVudABDcmVhdGVkIHdpdGggR0lNUFeBDhcAAAWZSURBVFjDxZd/SFRZFMe/M1iJs1nDJDkbVIoU79r8SBl1pD+SNsglaaslM//NxW0JNjaIIIUa2KCCsoXsjy11KVbUXaI1s3+MZCcynHG0bVJrCwfENzPNNE3NNPPezDv7R/p2JscfadCFA5d7zz33894995xzFZIkET5jUy50YTwe/yQAafNR8vv9GBoawuDgIJxOJxyOAYRCIWg0GhQUFMJoNECn04MxhvT09I8CUMx2BC6XCx0d7WhqagLR3Cel1WpRW/s9duzYAZVKtXAAQRDQ0dGB06d/ljfOy8vDzp0VyMvLw4oVK5CWloZ3797B7/ejv78ff/75BwRBAADk5ubi5MlT2Lx589wEkiRRorx69YqOHv2JGOOIMY4OHz5MAwMDJAgCfaibKIFAgG7evEllZVvltW1tbRSLxWZdlwTw+vVrqq2tJcY4MhgMdPv2bRJFcVYDH4rP5yOLxSJDXL9+fX4AoihSXV0dMcaR2WymR48efdTGiSKKIl258qsMcffu3bkBurq6iDGO8vMZDQwMLHjzKYnFYnThwgVijKOSkmKamJiYGcDv91NpaSkxxlFr6++L3nxKQqEQVVcfIMY4OnPmzMwA7e1txBhHu3d/Q+Fw+JMBSJJENptNPgqXyzVtXhmLxdDS8hsAoKbmuxkDiSAI4Hkedrsd3d3dGB8fn9c9NxgMKCkpAQBYrdbpkdDlcuH5838BACaTKWkyFAqhubkZTqcTVuvfiMViMBqNmJiYwMWLv2DNmjVzx3qlEnv3fosHDx7g1q1OVFZWJgO8ePECAGA2l0Kj0SRNRiIRNDZewuXLl3H8+HGo1WqoVCqcOHFC1unr60sZJVevXo2cnBwAAMdxAAC73Y5AIICVK1f+D8DzPABAp9s041cUFBQiIyMj5Vw4HE4JMBUVAWDVqlVyfxqAx+OZjONfLiibabXalACZmZlyPxE+GAwmHwGRtKh0Ojo6mhIgNzcHWq32fcJRKOTxD3XTsrKyAABut3tBAEVFJkzZVKvV8Pl805wzHA7L/eXLlycDZGe/p3Q6H8uDY2Nj4PkJqFRfJBlpamoCz/MYHR2Rxzs7b0GS3hcn27Z9BYfDAa/XA73eALPZDIVCAZ/PJ+ur1epkgPXr1wMAent7EQgEoFQqsWfPbkSj0aTf5nA40Nh4SR6bcqSDBw8mGZxyMrvdBo1Gg40bN2JkZGTS0fVJDggAynXr1sm/zGazIT09HUajUVY4cKAaKpUK+fn5KC//Ghs2bEBDw8UZY4BOp8OdO93geTfOnz+P4eFhZGZmIjtbi4qKnUn+INcD165dI8Y42r+/kiKRCL1584asVis9fPiQotHoR4ffeDxOPT09ZLGcIpvNRsFgkPx+P/E8nzoXuN1uKiwsIMY4unHjxifJAaIoktvtpuLiIqqqqqJnz57Nno47OtonCxE9OZ3ORQPE43F6+/Yt3bt3jxjjyGQyUX9//8wA0WiUjhz5kRjjqKxsKz19+nRRtUBrayvV19eTIAjU3NxMjHG0aVM+Wa3WmUuyly9fUlVV1WRVVEK9vb0Uj8c/avNgMCgXIoxxdP/+fRIEgc6dO0eMcaTX62hoaCg1gCRJ5PF4qKamRjZQV1dHw8PDc4KEQiHq6emhiooKee3Vq1flmjIajVJDQwMxxtH27dvJ5/ORJEmUsiyfSsOJ976oqBjl5eVYu3YtNBoNlixZglAoBK/Xi8eP/0F7ezu8Xu9k8smCxWLBli1bkq6dJEno6upCfX0d9u2rxLFjx2Z/mDx58gQtLS3o7PxrXmE5IyMDhw79gF27dk2LeIltbGwMZ8+eRXV19ewAiS+kwcFB9PX1wWq1wuv1JAWeoqJimEwm6PX6pCw4WxNFEePj4/MDSGxEhEgkglgshmXLlmHp0qWLyqaKz/08/w+mQbs0lsCj8gAAAABJRU5ErkJggg=='
+        root= tk.Tk()
+        top= root
+        top.geometry("600x450+468+138")
+        top.resizable(1,1)
+        top.title("Sound to Text")
+        favicon=tk.PhotoImage(data=img) 
+        root.wm_iconphoto(True, favicon)
+
+#Textbox
+def create_textbox():
+        global textbox
+        textbox = Text(top)
+        textbox.place(relx=0.033, rely=0.022, relheight=0.918, relwidth=0.933)
+        scroll_1=Scrollbar (top)
+        scroll_1.pack(side=RIGHT, fill=Y)
+        textbox.configure(yscrollcommand=scroll_1.set)
+        scroll_1.configure(command=textbox.yview)
+        textbox.configure(state=DISABLED)
+
+#Open File
+def open_file():
+    global soundfile
+    global soundfilename
+    data=[('WAV', '*.wav')]
+    soundfilename=askopenfilename(filetypes=data)
+    if str(soundfilename)!='':
+        soundfile=open(soundfilename,'rb')
+        encode()
+
+#encode
+def encode():
+    global soundtext
+    global tempfile
+    tempfile=open("tempfile",'w')
+    soundtext=''
+    soundfile.seek(44)
+    filename=str(soundfilename)
+    soundfile_size= os.path.getsize(filename)
+    for chars in range (1,soundfile_size):
+        bytea=soundfile.read(1)
+        bytevalue=int.from_bytes(bytea, "big")
+        char=chr(int(bytevalue/9.846153)+96)
+        if chars<20000:
+            soundtext=soundtext+char
+        tempfile.write(char)
+    textbox.configure(state=NORMAL)
+    textbox.delete(1.0,END)
+    textbox.insert(INSERT,soundtext[0:20000])
+    textbox.configure(state=DISABLED)
+
+#Copy Code
+def copy_text():
+    textbox.tag_add(SEL, "1.0", END)
+    textbox.event_generate(("<<Copy>>"))
+
+#Save to file
+def Save_to_file():
+        data=[('Text','*.txt')]
+        reportfile=asksaveasfilename(filetypes=data, defaultextension=data)
+        if str(reportfile)!='':
+            shutil.copy("tempfile",reportfile)
+        tempfile.close()
+        os.remove("tempfile")
+            
+
+#Quit
+def QuitApp():
+    okcancel= messagebox.askokcancel("Quit?","Do you want to quit the app?",default="ok")
+    if okcancel== True:
+        top.destroy()
+
+#menu
+def create_menu():
+    menubar=tk.Menu(top, tearoff=0)
+    top.configure(menu=menubar)
+    sub_menu=tk.Menu(top, tearoff=0)
+    menubar.add_cascade(menu=sub_menu,compound="left", label="File")
+    sub_menu.add_command(compound="left", label="Open", command=open_file, accelerator="Ctrl+O")
+    sub_menu.add_command(compound="left",label="Copy", command=copy_text, accelerator="Ctrl+C")
+    sub_menu.add_command(compound="left",label="Save", command=Save_to_file,accelerator="Ctrl+S")
+    sub_menu.add_command(compound="left",label="Quit", command=QuitApp, accelerator="Ctrl+Q")
+    top.bind_all("<Control-o>",open_file_hotkey)
+    top.bind_all("<Control-c>",copy_hotkey)
+    top.bind_all("<Control-s>",Save_hotkey)
+    top.bind_all("<Control-q>",Quit_hotkey)
+
+def open_file_hotkey(event):
+    open_file()
+
+def copy_hotkey(event):
+    copy_text()
+
+def Save_hotkey(event):
+    Save_to_file()
+
+def Quit_hotkey (event):
+    QuitApp()
+
+#contextmenu
+def context_menu(event):
+        try:
+                menucopy.tk_popup(event.x_root, event.y_root)
+        finally:
+                menucopy.grab_release()
+def create_context_menu():
+        global menucopy
+        root.bind("<Button-3>", context_menu)
+        menucopy = Menu(root, tearoff = 0)
+        menucopy.add_command(label="Copy", command=copy_text)
+
+#main procedure
+def main():
+    create_app_window()
+    create_textbox()
+    create_menu()
+    create_context_menu()
+
+
+main()
+root.mainloop()
